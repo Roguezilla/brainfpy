f3e4e60eb158120e85fe9e311d7c0e0b7aa507ed
Olek <23708759+Roguezilla@users.noreply.github.com>
2018-03-16 22:30:04 +0000

    LESS SIZE

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import sys,re
-c,f,_,b,d,p,e=[],open(sys.argv[1],'r'),[],{},0,0,[0 for i in range(9999)]
-for z in ''.join(filter(lambda x:x in '.,[]<>+-',re.sub('\n','',f.read()))):c.append(z)
-for p,m in enumerate(''.join(c)):
-  if m=='[':_.append(p)
-  if m==']':s=_.pop();b[s]=p;b[p]=s
-while d < len(c):
-  m=c[d]
-  if m=='>':p+=1
-  if m=='<':p-=1
-  if m=='+':e[p]+=1
-  if m=='-':e[p]-=1
-  if m=='.':sys.stdout.write(chr(e[p]))
-  if m==',':e[p]=ord(sys.stdin.read(1))
-  if m=='[' and e[p]==0:d=b[d]
-  if m==']' and e[p]!=0:d=b[d]
-  d+=1
+a,c,d,e,f,g,h=[],[],{},0,0,[0 for i in range(50)],sys
+for z in ''.join(filter(lambda x:x in '.,[]<>+-',re.sub('\n','',open(h.argv[1],'r').read()))):a.append(z)
+for p,m in enumerate(''.join(a)):
+  if m=='[':c.append(p)
+  if m==']':s=c.pop();d[s]=p;d[p]=s
+while e<len(a):
+  m=a[e]
+  if m=='>':f+=1
+  if m=='<':f-=1
+  if m=='+':g[f]+=1
+  if m=='-':g[f]-=1
+  if m=='.':h.stdout.write(chr(g[f]))
+  if m==',':g[f]=ord(h.stdin.read(1))
+  if m=='[' and g[f]==0:e=d[e]
+  if m==']' and g[f]!=0:e=d[e]
+  e+=1
