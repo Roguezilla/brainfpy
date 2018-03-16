a8f699badd6f78579ad9ff77b90b6ead07d39913
Olek <23708759+Roguezilla@users.noreply.github.com>
2018-03-16 21:35:53 +0000

    Soldier down

--- main2.py
+++ main2.py
@@ -1,37 +1,32 @@
 import sys, re
-def buildbracemap(code): 
-  temp_bracestack, bracemap = [], {}
-  for position, command in enumerate(code):
-    if command == "[": temp_bracestack.append(position)
-    if command == "]":
-      start = temp_bracestack.pop()
-      bracemap[start] = position
-      bracemap[position] = start
-  return bracemap
-def run(filename):
-  code = []
-  with open(filename, 'r') as f:
-    for each_char in ''.join(filter(lambda x: x in '.,[]<>+-', re.sub('\n', '', f.read()))):
-      code.append(each_char)
-  bracemap = buildbracemap(''.join(code))
-  cells, codeptr, cellptr = [0 for i in range(9999)], 0, 0
-  while codeptr < len(code):
-    command = code[codeptr]
-    if command == '>':
-      cellptr += 1
-    elif command == '<':
-      cellptr -= 1
-    elif command == '+':
-      cells[cellptr] = cells[cellptr] + 1
-    elif command == '-':
-      cells[cellptr] = cells[cellptr] - 1
-    elif command == '.':
-      sys.stdout.write(chr(cells[cellptr]))
-    elif command == ',':
-      cells[cellptr] = ord(sys.stdin.read(1))
-    elif command == '[' and cells[cellptr] == 0:
-      codeptr = bracemap[codeptr]
-    elif command == ']' and cells[cellptr] != 0:
-      codeptr = bracemap[codeptr]
-    codeptr += 1
-run(sys.argv[1])
+c = []
+f = open(sys.argv[1], 'r')
+for e in ''.join(filter(lambda x: x in '.,[]<>+-', re.sub('\n', '', f.read()))):
+  c.append(e)
+_, b = [], {}
+for p, m in enumerate(''.join(c)):
+  if m == "[": _.append(p)
+  if m == "]":
+    s = _.pop()
+    b[s] = p
+    b[p] = s
+e, d, p = [0 for i in range(9999)], 0, 0
+while d < len(c):
+  m = c[d]
+  if m == '>':
+    p += 1
+  if m == '<':
+    p -= 1
+  if m == '+':
+    e[p] = e[p] + 1
+  if m == '-':
+    e[p] = e[p] - 1
+  if m == '.':
+    sys.stdout.write(chr(e[p]))
+  if m == ',':
+    e[p] = ord(sys.stdin.read(1))
+  if m == '[' and e[p] == 0:
+    d = b[d]
+  if m == ']' and e[p] != 0:
+    d = b[d]
+  d += 1
