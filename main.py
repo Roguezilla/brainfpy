170f8a29998122233fab3458d9c138ecd986d5ec
Olek <23708759+Roguezilla@users.noreply.github.com>
2018-03-17 09:06:10 +0000

    487 bytes boys

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import sys,re
+exec("""import sys,re
 a,c,d,e,f,g,h=[],[],{},0,0,[0 for i in range(50)],sys
-for z in ''.join(filter(lambda x:x in '.,[]<>+-',re.sub('\n','',open(h.argv[1],'r').read()))):a.append(z)
+for z in ''.join(filter(lambda x:x in '.,[]<>+-',re.sub('\\n','',open(h.argv[1],'r').read()))):a.append(z)
 for p,m in enumerate(''.join(a)):
-  if m=='[':c.append(p)
-  if m==']':s=c.pop();d[s]=p;d[p]=s
+Y[':c.append(p)
+Y]':s=c.pop();d[s]=p;d[p]=s
 while e<len(a):
   m=a[e]
-  if m=='>':f+=1
-  if m=='<':f-=1
-  if m=='+':g[f]+=1
-  if m=='-':g[f]-=1
-  if m=='.':h.stdout.write(chr(g[f]))
-  if m==',':g[f]=ord(h.stdin.read(1))
-  if m=='[' and g[f]==0:e=d[e]
-  if m==']' and g[f]!=0:e=d[e]
-  e+=1
+Y>':f+=1
+Y<':f-=1
+Y+':K+=1
+Y-':K-=1
+Y.':h.stdout.write(chr(K))
+Y,':K=ord(h.stdin.read(1))
+Y[' and K==0:e=d[e]
+Y]' and K!=0:e=d[e]
+  e+=1""".replace('Y',"  if m=='").replace('K','g[f]'))
