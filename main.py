4d934af7869f4fe70c8a5daa5d51a743dfc36066
Olek <23708759+Roguezilla@users.noreply.github.com>
2018-03-22 18:19:38 +0000

    495?

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-import sys,re
-a,c,d,e,f,g,h=[],[],{},0,0,[0 for i in range(999)],sys
-for z in ''.join(filter(lambda x:x in'.,[]<>+-',open(h.argv[1],'r').read())):a.append(z)
-for p,m in enumerate(''.join(a)):
-  if m=='[':c.append(p)
-  if m==']':s=c.pop();d[s]=p;d[p]=s
-while e<len(a):
-  m=a[e]
-  if m=='>':f+=1
-  if m=='<':f-=1
-  if m=='+':g[f]+=1
-  if m=='-':g[f]-=1
-  if m=='.':h.stdout.write(chr(g[f]))
-  if m==',':g[f]=ord(h.stdin.read(1))
-  if m=='[' and g[f]==0:e=d[e]
-  if m==']' and g[f]!=0:e=d[e]
-  e+=1
+import sys,rea,c,d,e,f,g,h=[],[],{},0,0,[0 for i in range(999)],sysfor z in ''.join(filter(lambda x:x in'.,[]<>+-',open(h.argv[1],'r').read())):a.append(z)for p,m in enumerate(''.join(a)):  if m=='[':c.append(p)  if m==']':s=c.pop();d[s]=p;d[p]=swhile e<len(a):  m=a[e]  if m=='>':f+=1  if m=='<':f-=1  if m=='+':g[f]+=1  if m=='-':g[f]-=1  if m=='.':h.stdout.write(chr(g[f]))  if m==',':g[f]=ord(h.stdin.read(1))  if m=='[' and g[f]==0:e=d[e]  if m==']' and g[f]!=0:e=d[e]  e+=1
