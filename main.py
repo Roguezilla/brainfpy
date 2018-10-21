8161c00db0717e942a2927e9a2d66e15f951f650
Olek <23708759+Roguezilla@users.noreply.github.com>
2018-10-21 18:20:45 +0100

    ungay

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-exec('''import sysd=e=z=0a,f=input(),[0]*999while d<len(a):	m=a[d]A'>':e+=1A'<':e-=1A'+':f[e]+=1A'-':f[e]-=1A'.':sys.stdout.write(chr(f[e]))A',':f[e]=ord(sys.stdin.read(1))A'[':z=dA']' and f[e]!=0:d=z	d+=1'''.replace('A','\n	if m=='))
+exec('''import sys
+d=e=z=0
+a,f=input(),[0]*999
+while d<len(a):
+	m=a[d]A'>':e+=1A'<':e-=1A'+':f[e]+=1A'-':f[e]-=1A'.':sys.stdout.write(chr(f[e]))A',':f[e]=ord(sys.stdin.read(1))A'[':z=dA']' and f[e]!=0:d=z
+	d+=1'''.replace('A','\n	if m=='))
