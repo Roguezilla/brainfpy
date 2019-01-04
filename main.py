f31d0446b63d04b4a8c8028adf69e0e9f34df593
Olek <23708759+Roguezilla@users.noreply.github.com>
2019-01-04 22:15:04 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-exec('''import sys
+exec('''
 d=e=z=0
 a,f=input(),[0]*999
 while d<len(a):
-	m=a[d]A'>':e+=1A'<':e-=1A'+':f[e]+=1A'-':f[e]-=1A'.':sys.stdout.write(chr(f[e]))A',':f[e]=ord(sys.stdin.read(1))A'[':z=dA']' and f[e]!=0:d=z
+	m=a[d]A'>':e+=1A'<':e-=1A'+':f[e]+=1A'-':f[e]-=1A'.':print(chr(f[e]),end='')A',':f[e]=ord(input())A'[':z=dA']' and f[e]!=0:d=z
 	d+=1'''.replace('A','\n	if m=='))
