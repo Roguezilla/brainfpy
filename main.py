5fdd4002934046436ea2085f1ef176c9a3c3cc6a
Olek <23708759+Roguezilla@users.noreply.github.com>
2019-01-04 22:15:47 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-exec('''
-d=e=z=0
+exec('''d=e=z=0
 a,f=input(),[0]*999
 while d<len(a):
 	m=a[d]A'>':e+=1A'<':e-=1A'+':f[e]+=1A'-':f[e]-=1A'.':print(chr(f[e]),end='')A',':f[e]=ord(input())A'[':z=dA']' and f[e]!=0:d=z
