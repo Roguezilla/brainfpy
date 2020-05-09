815ff72532cfa22bf5aa79b6a1b47cc93f8ae3bc
Olek <roguezilla0@gmail.com>
2020-05-09 09:32:42 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 exec('''d=e=z=0
 a,f=input(),[0]*999
 while d<len(a):
-	m=a[d]A'>':e+=1A'<':e-=1A'+':f[e]+=1A'-':f[e]-=1A'.':print(chr(f[e]),end='')A',':f[e]=ord(input())A'[':z=dA']' and f[e]!=0:d=z
-	d+=1'''.replace('A','\n	if m=='))
+	m=a[d]A>':e+=1A<':e-=1A+':f[e]+=1A-':f[e]-=1A.':print(chr(f[e]))A,':f[e]=ord(input())A[':z=dA]'and f[e]!=0:d=z
+	d+=1'''.replace("A","\n	if m=='"))
