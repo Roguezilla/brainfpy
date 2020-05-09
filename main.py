0af4d21f850ee20707cd2936991c10db595bf381
zax2002 <zax2002@yandex.ru>
2020-05-09 16:44:42 +0300

    191 bytes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 exec('''d=e=z=0
 a,f=input(),[0]*999
-while d<len(a):	A>':e+=1A<':e-=1A+':f[e]+=1A-':f[e]-=1A.':print(chr(f[e]),end='')A,':f[e]=ord(input())A[':z=dA]'and f[e]:d=z
+while d<len(a):A>':e+=1A<':e-=1A+':f[e]+=1A-':f[e]-=1A.':print(chr(f[e]))A,':f[e]=ord(input())A[':z=dA]'and f[e]:d=z
 	d+=1'''.replace('A',"\n	if a[d]=='"))
