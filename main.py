757a9a1251949e3c2e5a7b2f5e5386fe0050c767
Olek <roguezilla0@gmail.com>
2020-11-29 20:05:46 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 z=a=x=0
 c,m=input(),[0]*999
-while(z<len(c)):exec("a+=1|a-=1|m[a]+=1|m[a]-=1|print(chr(m[a]))|m[a]=ord(input())|x=z|if m[a]:z=x".split("|")["><+-.,[]".find(c[z])]);z+=1
+while z<len(c):exec("a+=1|a-=1|m[a]+=1|m[a]-=1|print(chr(m[a]))|m[a]=ord(input())|x=z|if m[a]:z=x".split("|")["><+-.,[]".find(c[z])]);z+=1
